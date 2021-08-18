108683185887426bea0882f5b56ad84cadeb0352
Casey Harding <casey.harding@generalassemb.ly>
2021-08-18 13:23:24 -0400

    Finished codealong.

--- src/components/App.js
+++ src/components/App.js
@@ -73,8 +73,16 @@ export default function App() {
   //////////////// EVENT HANDLERS ////////////////
   //////////////// EVENT HANDLERS ////////////////
   //////////////// EVENT HANDLERS ////////////////
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
+
   const inputChange = (name, value) => {
     // 🔥 STEP 10- RUN VALIDATION WITH YUP
+    validate(name, value)
     setFormValues({
       ...formValues,
       [name]: value // NOT AN ARRAY
@@ -110,7 +118,7 @@ export default function App() {
   useEffect(() => {
     // 🔥 STEP 9- ADJUST THE STATUS OF `disabled` EVERY TIME `formValues` CHANGES
     schema.isValid(formValues).then(valid => setDisabled(!valid))
-  }, [])
+  }, [formValues])
 
   return (
     <div className='container'>
