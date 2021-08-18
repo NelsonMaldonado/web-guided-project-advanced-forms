fbc76cb6ad575836eba82f5fb68ea1c4abfb71ab
Casey Harding <casey.harding@generalassemb.ly>
2021-08-18 12:49:30 -0400

    Through step 6.

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ import FriendForm from './FriendForm';
 // 🔥 STEP 3- FLESH THE SCHEMA IN ITS OWN FILE
 // 🔥 STEP 4- IMPORT THE SCHEMA, AXIOS AND YUP
 
+import schema from '../validation/formSchema';
+import axios from 'axios';
+import * as yup from 'yup';
+
 
 //////////////// INITIAL STATES ////////////////
 //////////////// INITIAL STATES ////////////////
@@ -48,12 +52,22 @@ export default function App() {
   const getFriends = () => {
     // 🔥 STEP 5- IMPLEMENT! ON SUCCESS PUT FRIENDS IN STATE
     //    helper to [GET] all friends from `http://buddies.com/api/friends`
+    axios.get('http://buddies.com/api/friends')
+      .then(res => {
+        setFriends(res.data);
+      }).catch(err => console.error(err))
   }
 
   const postNewFriend = newFriend => {
     // 🔥 STEP 6- IMPLEMENT! ON SUCCESS ADD NEWLY CREATED FRIEND TO STATE
     //    helper to [POST] `newFriend` to `http://buddies.com/api/friends`
     //    and regardless of success or failure, the form should reset
+    axios.post('http://buddies.com/api/friends', newFriend)
+      .then(res => {
+          setFriends([res.data, ...friends]);
+      }).catch(err => console.error(err));
+
+    setFormValues(initialFormValues);
   }
 
   //////////////// EVENT HANDLERS ////////////////
